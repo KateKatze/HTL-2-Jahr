439f4d6fb916511f8a94674f488fa0cf5e548cce
KateKatze <dr.katekogler@gmail.com>
2022-09-30 21:00:11 +0200

    Tag 2.

---
+++ IOCheck/src/at/htlpinkafeld/io/IOCheck.java
@@ -0,0 +1,77 @@
+package at.htlpinkafeld.io;
+
+import java.util.Scanner;
+
+public class IOCheck {
+        /*
+        public static void main(String[] args) {
+            //int x = 5;
+            //System.out.println("Expression: " + x);
+            //System.out.println("Expression: " + 2 + 1); Expression 21
+            //System.out.println("Expression: " +( 2 + 1)); Expression 3
+            //System.out.println("Expression: " + 2 * 2); Expression 4
+            //System.out.print("Expression: " );
+            //System.out.println((float)x/2);
+
+            int age = 5;
+            Scanner input = new Scanner(System.in);  //wartet, bis der User in der Console ein int eingibt
+            System.out.print("Eingabe: age = ");
+            age = input.nextInt();
+            System.out.println("Age: " + age);
+            //System.out.println("Age: " + (double) age); kommt mit .0 aus -> 12.0
+        }
+
+        public static void main(String[] args) {
+            int i;
+            int fakultzahl;
+            int faktorial = 1;
+            Scanner input = new Scanner(System.in);
+            System.out.print("Ausgabe aller Faktoriellen bis: ");
+            fakultzahl = input.nextInt();
+            for (i=1; i <= fakultzahl; i++) {
+                faktorial = faktorial * i; // oder faktorial *= i
+                System.out.println(fakultzahl + "! = " + faktorial);
+            }
+        }
+
+    public static void main(String[] args) {
+        int upper;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Berechnen der Faktoriellen von: ");
+        upper = input.nextInt();
+        System.out.println(upper + "! = " + fakt(upper));
+    }
+
+    public static int fakt (int upper) {
+        int f = 1;
+        for (int n=2; n<=upper; n++ ) {
+            f *= n;
+        }
+        return f;
+    }
+
+         */
+ static  Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int upper;
+
+        upper = readInt("Berechnen der Faktorielle von: ");
+        System.out.println(upper + "! = " + fakt(upper));
+    }
+
+    public static int readInt(String txt) {
+        int retVal;
+        System.out.print(txt);
+        retVal = input.nextInt();
+        return retVal;
+    }
+
+    public static int fakt (int upper) {
+        int f = 1;
+        for (int n=2; n<=upper; n++ ) {
+            f *= n;
+        }
+        return f;
+    }
+}
