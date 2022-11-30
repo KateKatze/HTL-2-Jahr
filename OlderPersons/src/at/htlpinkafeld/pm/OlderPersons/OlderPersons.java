8f6d062939579863eba938c8e49e5020127a1f31
KateKatze <dr.katekogler@gmail.com>
2022-11-30 20:01:18 +0100

    HÜ Older Persons almost done (review array pushing)

--- OlderPersons/src/at/htlpinkafeld/pm/OlderPersons/OlderPersons.java
+++ OlderPersons/src/at/htlpinkafeld/pm/OlderPersons/OlderPersons.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class OlderPersons {
     public static void main(String[] args) {
-        // array for ppl args.length = 2 (name, age)
-        Persons[] person = new Persons[args.length];
         // scanner
         Scanner input = new Scanner(System.in);
 
-        // checking arguments = 2
-        if (args.length != 2){
-            System.out.println("It supposed to be name and age!");
+        // creating array for ppl args.length / 2 // (name, age)
+        Persons[] person = new Persons[args.length/2];
+
+        // checking arguments = 2 for every person, when no - throwing an error
+        if (args.length%2 != 0) {
+            System.out.println("It supposed to be name and age for every person!");
             return;
         }
 
@@ -20,42 +21,43 @@ public class OlderPersons {
         }
 
         // printing out the list of people
-        System.out.println("The whole crowd:");
-        for (Persons p: person) {
-            System.out.println("\n" + p);
+        System.out.println("\n" + "The whole crowd:");
+        for (Persons per: person) {
+            System.out.println("\n" + per);
         }
 
         // printing the older ones
         double averageAge = getAverageAge(person);
-        System.out.println("The older ones (avg = " + averageAge + "):");
-        for (Persons p: person) {
-            if (p.getAge() > averageAge){
-                System.out.println("\n" + p);
+        System.out.println("\n" + "The older ones (avg = " + averageAge + "):");
+        for (Persons per: person) {
+            if (per.getAge() > averageAge){
+                System.out.println("\n" + per);
             }
         }
 
         // setting false to find the match with the name we have in the "base"
         boolean search = false;
-        System.out.print("Enter the name you are looking for: ");
+        System.out.println("\n" + "Enter the name you are looking for: ");
         String name = input.next();
-        for (Persons p: person){
-            if (name.equals(p.getName())){
+        for (Persons per: person){
+            if (name.equals(per.getName())){
                 if (!search){
                     System.out.println("I've found:");
                     search = true;
                 }
-                System.out.println("\n" + p);
+                System.out.println("\n" + per);
             }
         }
         if (!search){
             System.out.println("That person doesn´t exist");
         }
     }
+
     // checking the average age to print the older ones (>averageAge)
     public static double getAverageAge(Persons[] person){
         double ageTotal = 0;
-        for (Persons p: person){
-            ageTotal += p.getAge();
+        for (Persons per: person){
+            ageTotal += per.getAge();
         }
         return  ageTotal/(person.length);
     }
