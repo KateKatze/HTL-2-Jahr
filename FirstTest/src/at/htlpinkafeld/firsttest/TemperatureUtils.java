eaf92bd7703fb488b56060c31201a706b06d7502
KateKatze <dr.katekogler@gmail.com>
2023-03-10 20:53:54 +0100

    10.03.23 In der Klasse first testing

---
+++ FirstTest/src/at/htlpinkafeld/firsttest/TemperatureUtils.java
@@ -0,0 +1,7 @@
+package at.htlpinkafeld.firsttest;
+
+public class TemperatureUtils {
+    public static float celsiusToFahrenheit(float celsius) {
+        return celsius*5/9+32;
+    }
+}
---
+++ FirstTest/test/at/htlpinkafeld/firsttest/TemperatureUtilsTest.java
@@ -0,0 +1,17 @@
+package at.htlpinkafeld.firsttest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureUtilsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+    }
+}
