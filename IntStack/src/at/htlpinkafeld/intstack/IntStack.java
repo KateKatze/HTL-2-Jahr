d9d8e382e7083931caf65c0880d2fa57a3206ca7
KateKatze <dr.katekogler@gmail.com>
2023-03-11 16:32:27 +0100

    11.03.2023 in der Klasse

--- IntStack/src/at/htlpinkafeld/intstack/IntStack.java
+++ IntStack/src/at/htlpinkafeld/intstack/IntStack.java
@@ -1,13 +1,33 @@
 package at.htlpinkafeld.intstack;
 
-public class IntStack {
-        public IntStack(){
+import java.util.LinkedList;
+import java.util.List;
 
+public class IntStack {
+        private int size;
+        private List<Integer> list;
+
+        public IntStack(){
+               this(1);
         }
         public IntStack(int size){
-
+                if(size < 1)
+                        size=1;
+                this.size = size;
+                this.list = new LinkedList<>();
         }
         public int getSize(){
-            return -1;
+                return this.size;
+        }
+        public boolean isFull(){
+                return this.list.size() >= this.size;
+        }
+        public void push(int val){
+                if(this.isFull())
+                        throw new ArrayIndexOutOfBoundsException("Stack is full");
+                this.list.add(val);
+        }
+        public int getEleCnt(){
+                return -1;
         }
 }
--- IntStack/test/at/htlpinkafeld/intstack/IntStackTest.java
+++ IntStack/test/at/htlpinkafeld/intstack/IntStackTest.java
@@ -1,6 +1,7 @@
 package at.htlpinkafeld.intstack;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,7 +10,34 @@ class IntStackTest {
     private IntStack tenStack;
     @BeforeEach
     void setUp() {
-        minStack = new IntStack();
-        tenStack = new IntStack(10);
+            minStack = new IntStack();
+            tenStack = new IntStack(10);
+    }
+
+    @Test
+    void testSize(){
+            assertEquals(1, minStack.getSize(), "min size");
+            assertEquals(10, tenStack.getSize(), "10 size");
+            IntStack nullStack = new IntStack(0);
+            assertEquals(1, nullStack.getSize(), "null stack");
+    }
+
+    @Test
+    void testIsFull(){
+            assertFalse(minStack.isFull());
+            minStack.push(55);
+            assertTrue(minStack.isFull());
+    }
+    @Test
+    void testPushOverflow(){
+            minStack.push(10);
+           // minStack.push(11);
+            assertThrows(ArrayIndexOutOfBoundsException.class, () -> minStack.push(11), "throw Exception"); // ()->funktion aufruf
+    }
+
+    @Test
+    void testGetEleCnt(){
+        minStack.getSize();
+        assertEquals(1, minStack.getSize);
     }
 }
