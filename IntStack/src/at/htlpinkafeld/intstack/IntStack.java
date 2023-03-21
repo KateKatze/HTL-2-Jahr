df53c80e3ab5978d6fef599e772763d9b6219671
KateKatze <dr.katekogler@gmail.com>
2023-03-21 20:48:57 +0100

    HÜ done

--- IntStack/src/at/htlpinkafeld/intstack/IntStack.java
+++ IntStack/src/at/htlpinkafeld/intstack/IntStack.java
@@ -6,31 +6,57 @@ import java.util.List;
 public class IntStack {
         private int size;
         private List<Integer> list;
-
-        public IntStack(){
-               this(1);
-        }
         public IntStack(int size){
                 if(size < 1)
                         size=1;
                 this.size = size;
                 this.list = new LinkedList<>();
         }
+        public IntStack(){
+               this(1);
+        }
         public int getSize(){
                 return this.size;
         }
+        public int getEleCnt(){
+                return list.size();
+        }
         public boolean isFull(){
                 return this.list.size() >= this.size;
+        }
+        public boolean isEmpty(){
+                return this.list.isEmpty();
         }
         public void push(int val){
                 if(this.isFull())
                         throw new ArrayIndexOutOfBoundsException("Stack is full");
                 this.list.add(val);
         }
-        public int getEleCnt(){
-                return list.size();
+        public int pop() {
+                // get the last element
+                // delete this element
+                if(this.list.isEmpty()){
+                        throw new ArrayIndexOutOfBoundsException();
+                }
+               return this.list.remove(getEleCnt() - 1);
         }
-        public boolean isEmpty(){
-                return this.list.size() = 0;
+        public int peek() {
+                // get the last element
+                // without deleting it
+                if(this.list.isEmpty())
+                        throw new ArrayIndexOutOfBoundsException();
+                return this.list.get(getEleCnt()- 1);
+        }
+        public boolean resize(int size){
+                //changes the stack size as long as no element is lost,
+                //returns true if the operation has been successfully executed
+                if (this.list.size() > size || size < 1)
+                        throw new IllegalArgumentException();
+                this.size = size;
+                return true;
+        }
+        @Override
+        public String toString() {
+                return this.list.toString();
         }
 }
--- IntStack/test/at/htlpinkafeld/intstack/IntStackTest.java
+++ IntStack/test/at/htlpinkafeld/intstack/IntStackTest.java
@@ -40,4 +40,54 @@ class IntStackTest {
         minStack.push(1);
         assertEquals(1, minStack.getEleCnt());
     }
+
+    @Test
+    void isEmpty() {
+        assertTrue(minStack.isEmpty());
+        minStack.push(19);
+        assertFalse(minStack.isEmpty());
+    }
+
+    @Test
+    void pop() {
+        assertEquals(0, minStack.getEleCnt());
+        minStack.push(10);
+        assertEquals(1, minStack.getEleCnt());
+        assertEquals(minStack.pop(), 10);
+        assertEquals(0, minStack.getEleCnt());
+    }
+
+    @Test
+    void peek() {
+        tenStack.push(3);
+        assertEquals(tenStack.peek(), 3);
+        assertThrows(ArrayIndexOutOfBoundsException.class,()->minStack.peek(),"Peek exception");
+    }
+
+    @Test
+    void resize() {
+        assertEquals(minStack.getSize(), 1);
+        assertTrue(minStack.resize(3));
+        assertEquals(minStack.getSize(), 3);
+        minStack.push(1);
+        minStack.push(2);
+        minStack.push(3);
+        assertThrows(IllegalArgumentException.class,()->minStack.resize(0));
+        assertEquals(minStack.pop(),3);
+        assertTrue(minStack.resize(2));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(tenStack.toString(), "[]");
+        tenStack.push(1);
+        tenStack.push(2);
+        tenStack.push(3);
+        tenStack.push(4);
+        tenStack.push(5);
+        assertEquals(tenStack.toString(), "[1, 2, 3, 4, 5]");
+        tenStack.pop();
+        tenStack.pop();
+        assertEquals(tenStack.toString(), "[1, 2, 3]");
+    }
 }
